212f3fef9b4a79dfdc95beaa6e68395562271c03
GarethPhillips11 <u17004162@tuks.co.za>
2019-10-19 15:32:47 +0200

    update

---
+++ INF272_Project/INF272_Project/ViewModels/DisasterReportVMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272_Project.ViewModels
+{
+    public class DisasterReportVMController : Controller
+    {
+        // GET: DisasterReportVM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
