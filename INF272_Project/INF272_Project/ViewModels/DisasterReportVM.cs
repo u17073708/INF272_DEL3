0bf8798ec9e85da61cbdb4f81869b44431578b84
GarethPhillips11 <u17004162@tuks.co.za>
2019-10-20 13:53:45 +0200

    reports

--- INF272_Project/INF272_Project/ViewModels/DisasterReportVM.cs
+++ INF272_Project/INF272_Project/ViewModels/DisasterReportVM.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using INF272_Project.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +9,23 @@ namespace INF272_Project.ViewModels
 {
     public class DisasterReportVM : Controller
     {
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+
+        public Disaster disaster { get; set; }
+        public List<IGrouping<string, ReportRecord>> results { get; set; }
         // GET: DisasterReportVM
         public ActionResult Index()
         {
             return View();
         }
+    }
+    public class ReportRecord
+    {
+        public string Name { get; set; }
+        public string DisasterDate { get; set; }
+        public int DamageLevel { get; set; }
+        public int DisasterID { get; set; }
+        public int CityID { get; set; }
     }
 }
--- INF272_Project/INF272_Project/Views/ReportController.cs
+++ INF272_Project/INF272_Project/Views/ReportController.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using INF272_Project.Models;
+using INF272_Project.ViewModels;
+
 
 namespace INF272_Project.Views
 {
     public class ReportController : Controller
     {
+       
         // GET: Report
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisasterReport()
+        {
+            DisasterReportVM vm = new DisasterReportVM();
+
+            
+
+            //Set default values for the FROM and TO dates
+            vm.DateFrom = new DateTime(2014, 12, 1);
+            vm.DateTo = new DateTime(2014, 12, 31);
+
+
+            return View(vm);
+
+
+        }
+
+        [HttpPost]
+        public ActionResult DisasterReport(DisasterReportVM vm)
+        {
+            using (Entities db = new Entities())
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+
+
+
+
+                List<Disaster> disasters = db.Disasters.ToList();
+
+                
+
+                
+                
+                TempData["records"] = disasters.ToList();
+                
+                return View(vm);
+            }
+
         }
     }
 }
