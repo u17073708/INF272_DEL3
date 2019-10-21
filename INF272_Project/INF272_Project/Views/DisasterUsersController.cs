084f47d954e26583be22695e7e250d030bb692f7
hansentheasian <hansentheasian2gmail.com>
2019-10-21 23:21:49 +0200

    get help and give help work

--- INF272_Project/INF272_Project/Views/DisasterUsersController.cs
+++ INF272_Project/INF272_Project/Views/DisasterUsersController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using INF272_Project.Models;
+using System.Net.Mail;
 
 namespace INF272_Project.Views
 {
@@ -20,6 +21,69 @@ namespace INF272_Project.Views
             var disasterUsers = db.DisasterUsers.Include(d => d.AppUser).Include(d => d.Disaster);
             return View(disasterUsers.ToList());
         }
+      
+
+        public ActionResult Index2()
+        {
+            var disasterUsers = db.DisasterUsers.Include(d => d.AppUser).Include(d => d.Disaster);
+            return View(disasterUsers.ToList());
+        }
+
+        public ActionResult Index3()
+        {
+            var disasterUsers = db.DisasterUsers.Include(d => d.AppUser).Include(d => d.Disaster);
+            return View(disasterUsers.ToList());
+        }
+
+      
+
+        public ActionResult GiveHelpForms(string name, string city, string days, string description, string btnSubmit)
+        {
+            if (btnSubmit == "Submit")
+            {
+                if (name == "" || city == "" || days == "" || description == "")
+                {
+
+                    ViewBag.Success = "Please complete all fields";
+                    return View("GiveHelpForms");
+                }
+                else
+                {
+                    ViewBag.Success = "Email sent to the person needing help!";
+                    var senderEmail = new MailAddress("u17073708@tuks.co.za"); //your email goes here
+                    var receiverEmail = new MailAddress("hansentheasian@gmail.com", "Administrator");
+                    var password = "Dragonoid3316_"; //your email password goes here
+                    var sub = "Help is on its way!";
+                    var body = "We hope this message finds you well, " + name + " is on their way and they will be there in " + days + " days because they are coming from " + city + "." + "\n" + "They will be providing you with the following help:" + "\n" + description + "\n" + "Hang in there as help is on its way!";
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com", //this might change accordingly
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = sub,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+
+                    return View("GiveHelpForms");
+
+                }
+            }
+            else
+            {
+                return View("GiveHelpForms");
+            }
+        }
+
+       
 
         // GET: DisasterUsers/Details/5
         public ActionResult Details(int? id, int? id2)
@@ -92,7 +156,7 @@ namespace INF272_Project.Views
             {
                 db.Entry(disasterUser).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index3");
             }
             ViewBag.UserID = new SelectList(db.AppUsers, "ID", "Name", disasterUser.UserID);
             ViewBag.DisasterID = new SelectList(db.Disasters, "ID", "ID", disasterUser.DisasterID);
@@ -122,7 +186,7 @@ namespace INF272_Project.Views
             DisasterUser disasterUser = db.DisasterUsers.Find(id2, id);
             db.DisasterUsers.Remove(disasterUser);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index3");
         }
 
         protected override void Dispose(bool disposing)
